3b2455650d31245f8f5072161cb7ee1e12c30bb0
Tim Smith <meckaleck@hotmail.com>
2023-01-31 21:27:10 -0600

    1/31 night push I have 1 question now showing but, no awnsers. praying my tutor can help after class tomorrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,41 +10,100 @@
 // end game when timer is done or all questions are awnsered correctly
 // save score 
 
-var startBtn = document.querySelector("#start");
-var timerSpan = document.querySelector("#timer");
+var startBtn = document.querySelector(".start");
+var timerSpan = document.querySelector(".timer");
 var timer = 3; 
-var nextBtn = "";
+
 var questions = [
     {
       question: "Commmonly used data type DO NOT include?",
-      answers: {
-        a: 'strings',
-        b: 'alerts',
-        c: 'boolean',
-        d: 'numbers'
-      },
-      correctAnswer: 'b'
+      choices: ['strings', 'alerts', 'boolean', 'numbers'],
+      correctAnswer: 'alerts',
     },
     {
       question: "The condition in a if/else statement is enclosed within _?",
-      answers: {
-        a: 'quotes',
-        b: 'parentheses',
-        c: 'curly brackets',
-        d: 'underscores'
-      },
-      correctAnswer: 'c'
+      choices: ['quotes', 'parentheses', 'curly brackets', 'underscores'],
+      correctAnswer: 'curly brackets',
+    }, 
+    { question: "What does CSS stand for?",
+      choices: ['Calm Sleeping Cats', 'Crazy Sea Shells', 'Corn Stalk Strands', 'Cascading Style Sheets'],
+      correctAnswer: 'Cascading Style Sheets',  
     }
   ];
 
 startBtn.addEventListener("click", function () {
-    var gameTimer = setInterval(function () {
+  document.querySelector('.questions').textContent=questions[0].question;
+  
+  //document.querySelector('.btns').textContent=choices;
+  var gameTimer = setInterval(function () {
         timer--;
         timerSpan.textContent = timer;
         if (timer <= 0) {
             clearInterval(gameTimer)
         }
-
-    }, 1000)
+    }, 1000);
+    showQuestions();
 })
 
+function showQuestions(event) {
+  document.querySelector('.questions')
+  //event.preventDefault();
+  //console.log(event)
+}
+
+// ideally hide and unhide question cycyling through
+
+//////junkyard///////
+// Array.from(questions).forEach(function(questions) {
+//   element.addEventListener('click', myFunction);
+// });
+
+
+
+////////////////////////
+
+//Psuedo code porvided bt Sam Adams TA///
+//click button to start     /
+    //event listener       /
+    //a timer will start  /
+//user is presented with a question
+//user selects one multiple choice question
+    //IF answer is wrong, decrement the timer
+        //Display that the user choice was incorrect, then move to next question
+    //IF answer is correct
+        //Display that the user choice was correct(textContent)
+        //Increment the score++
+    //FOR loop to loop through the questions array
+//When timer hits 0 OR no more questions in array
+    //THEN clear interval
+    //THEN allow user to input initials
+    //THEN link to highscores page(two separate HTML pages, index.html and hiscores.html)
+    //Save data to local storage(setItem, getItem)
+
+//startQuiz() function changes content
+    //hide the start screen
+    //unhide the questions section
+    //start the timer
+    //show the starting time
+    //call getQuestion()
+//getQuestion() function get current question object from questions array
+    //change content of the page
+    //getElementbyId, change textContent
+    //loop over choices create a button for each choice
+    //after creating all the elements we appendChild
+//questionClick() function that checks choice user clicked
+    //first we check that the event.target matches an answer choice
+    //check event.target.value matches the questions[currentIndex].answer
+
+  //   var questions = [
+  //     {
+  //         question: "What does HTML stand for?",
+  //         choices: ["Hyper text Martian language", "Hyper text markup language"],
+  //         answer: "Hyper text markup language",
+  //     },
+  //     {
+  //         question: "What does CSS stand for?",
+  //         choices: ["Cats stay sleeping", "Cascading style sheets"],
+  //         answer: "Cascading style sheets",
+  //     }
+  // ]
